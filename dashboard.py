86ec4fdd2f17792bad810daf5a0db19373f521e0
antony0315 <68135399+antony0315@users.noreply.github.com>
2022-05-08 22:12:00 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,7 @@ from functools import reduce
 import scipy.optimize as solver
 
 
-tw100=pd.read_csv("https://raw.githubusercontent.com/antony0315/stockprofolio/main/TW100.csv?token=GHSAT0AAAAAABUK4YUE634NGOMPMFMLG332YTXZV5A",header=None)
+tw100=pd.read_csv('https://raw.githubusercontent.com/antony0315/stockprofolio/main/TW100.csv?token=GHSAT0AAAAAABUK4YUE634NGOMPMFMLG332YTXZV5A',header=None)
 
 st.title("效率前緣資產組合配置")
 st.markdown("作者:Antony JHU")
